8786e63a521741d4df6a0320bbac750fc295ab1b
dreamxd12356 <dreamxd6090@gmail.com>
2025-08-04 20:07:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,10 @@ if section == "📂 Upload & Analyze":
             ax.set_title("Predicted Alert Level Distribution")
             st.pyplot(fig)
         else:
-            st.error("❌ Missing required columns. Please upload a complete file.")
+            missing_cols = [col for col in required_cols if col not in df.columns]
+            st.warning(f"""⚠️ Your uploaded file is missing one or more required columns.Please make sure your CSV includes **all** of the following columns:```text
+{', '.join(required_cols)}
+
 
 # Single Prediction Page
 elif section == "🚨 Single Prediction":
@@ -167,3 +170,4 @@ elif section == "⚙️ Settings":
     theme_choice = st.radio("Choose Theme", ["light", "dark"], index=0 if theme == "light" else 1)
     st.session_state.theme = theme_choice
     st.markdown(f"<h4 style='color:yellow;'>✅ Theme set to: <b>{theme_choice.upper()} MODE</b></h4>", unsafe_allow_html=True)
+
